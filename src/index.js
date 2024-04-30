f5fa196897c914e6da1cf7503385d0d8c092b8f2
metaory <metaory@gmail.com>
2024-04-30 09:58:33 +0800

    clean

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import defaultConfig from './config.js'
 import exec from './exec.js'
 
 export default function Pcall(...opts) {
-  if (new.target) return (f, ...args) => exec(opts.at(0), f, args)
+  if (new.target) return (f, ...a) => exec(opts.at(0), f, a)
   const [fn, ...args] = opts
   return exec(defaultConfig, fn, args)
 }
