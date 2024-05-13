cc019594501c46b1bd8dbe77a7756caf1bd2386d
metaory <metaory@gmail.com>
2024-05-14 01:01:11 +0800

    0.4.4

---
+++ test/pcall.spec.js
@@ -0,0 +1,51 @@
+/* eslint no-unused-vars: "off" */
+import test from 'node:test'
+import Pcall from '../src/index.js'
+
+import { readFile } from 'node:fs/promises'
+
+const { log: l } = console
+
+const job = (name, family) =>
+  new Promise(resolve => resolve(`Hi ${name} :: ${family} !`))
+
+test('simple', async () => {
+  // const filePath = '../../package.json'
+  // import Pcall from 'pcall.js'
+  const filePath = './package.json'
+  const readOpts = { encoding: 'utf8' }
+  const [err, res] = await Pcall(readFile, filePath, readOpts)
+
+  console.error('Pcall [ERR]:', err)
+  console.error('Pcall [RES]:', res)
+  // assert.ok(res)
+})
+
+test('advance', async () => {
+  const filePath = '../../package.json'
+  const readOpts = { encoding: 'utf8' }
+
+  const pcall = new Pcall({
+    onSuccess: (args, res) => {
+      /*···*/
+    },
+    onFailure: (args, err) => {
+      /*···*/
+    },
+    transformOnSuccess: (args, res) => {
+      /*·🚧·*/
+    },
+    transformOnFailure: (args, err) => {
+      /*·🚧·*/
+    },
+    cleanup: (args, res) => {
+      /*···*/
+    },
+    trace: true,
+  })
+
+  const [err, res] = await pcall(readFile, filePath, readOpts)
+  console.error('Pcall [ERR]:', err)
+  console.error('Pcall [RES]:', res)
+  assert.ok(res)
+})
