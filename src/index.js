64c8cb89aa2649cdf64b6a1f7a707c3daad16f35
metaory <metaory@gmail.com>
2024-06-10 07:14:25 +0800

    added timeout reject opt

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-class Pcall {
-  #tick
-  #span
-  #func
+import { setTimeout } from 'timers/promises'
 
+export class TimeoutError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = 'TimeoutError'
+  }
+}
+
+class Pcall {
   constructor(opt = {}) {
     ;[
       'onSuccess',
@@ -19,69 +24,71 @@ class Pcall {
 
   [Symbol.iterator] = function* () {
     yield true
-    yield new TypeError('NO AWAIT')
+    yield new TypeError('NO_AWAIT')
   }
 
+  #ack = x => {
+    if (x instanceof TimeoutError) throw x
+    return x
+  }
+  #func
+  #span
+  #tick
   noTrace = true
   noError = false
-
-  transformOnSuccess(args, res = {}) {
-    return res
-  }
-
-  transformOnFailure(args, err) {
-    return err
-  }
-
-  onSuccess(args, res) {
-    console.info('@OK', { args, res })
-  }
-
-  onFailure(args, err) {
-    console.error('@NO', { args, err })
-  }
-
+  timeout = 3_000
+  transformOnSuccess = res => res
+  transformOnFailure = err => err
+  onSuccess = res => console.info('@OK', res)
+  onFailure = err => console.error('@NO', err)
   onFinally(args, func, span) {
-    console.debug('[OK]: fulfilled', func, args, 'in', span, 's')
+    console.debug('[OK]: Finally Fulfilled', func, args, 'in', span, 's')
     args.name = 'PCALL::TRACE'
     args.message ??= 'N/A'
     Error.captureStackTrace(args)
     this.noTrace || void console.trace(args.stack)
   }
 
-  async exec(fn, ...args) {
-    try {
-      this.#tick = performance.now()
-      this.#func = fn.name
+  exec(fn, ...args) {
+    this.#tick = performance.now()
+    this.#func = fn.name
 
-      if (this.args) args.unshift(this.args)
+    if (this.args) args.unshift(this.args)
 
-      const res = await fn(...args)
+    const ac = new AbortController()
+    const signal = ac.signal
+    const timer = setTimeout(this.timeout, new TimeoutError(), { signal })
 
-      void this.onSuccess(args, res)
-
-      const out = this.transformOnSuccess(args, res)
-
-      return this.noError ? out : [false, out]
-    } catch (err) {
-      void this.onFailure(args, err)
-
-      const out = this.transformOnFailure(args, err)
-
-      return this.noError ? out : [true, out]
-    } finally {
-      this.#span = +((performance.now() - this.#tick) / 1_000).toFixed(2)
-      void this.onFinally(args, this.#func, this.#span)
-    }
+    return new Promise(resolve => {
+      Promise.race([fn(...args), timer])
+        .then(this.#ack)
+        .then(this.transformOnSuccess)
+        .then(res => {
+          resolve([false, res])
+          return res
+        })
+        .then(this.onSuccess)
+        .catch(this.transformOnFailure)
+        .then(err => {
+          resolve([true, err])
+          return err
+        })
+        .then(this.onFailure)
+        .finally(() => {
+          ac.abort()
+          this.#span = +((performance.now() - this.#tick) / 1_000).toFixed(2)
+          void this.onFinally(args, this.#func, this.#span)
+        })
+    })
   }
 }
 
-export default function (...o) {
+export default function (...opts) {
   if (new.target) {
-    const pcall = new Pcall(...o)
+    const pcall = new Pcall(...opts)
     return (fn, ...args) => pcall.exec(fn, ...args)
   }
-  const [fn, ...args] = o
+  const [fn, ...args] = opts
   const pcall = new Pcall()
   return pcall.exec(fn, ...args)
 }
